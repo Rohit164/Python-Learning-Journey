bce434fa73bd24349dd5a09560aaa79fe26666e5
Rohit Patil <patilrohit172004@gmail.com>
2026-09-11 21:02:45 +0530

    Creates one more important feature into 'Fake News Headline Generator' project which is give an option to user to download all generated headline in text file.

--- Project/Fake News Headline Generator/index.py
+++ Project/Fake News Headline Generator/index.py
@@ -1,5 +1,11 @@
+# Create a Fake News Headline Generator Python Project
+
+
+# Import random module
 import random
 
+
+# Create three pre-defined lists - names, actions, places .  
 names = [
             'Shahrukh Khan',
             'Nirmala Sitaraman',
@@ -20,7 +26,8 @@ actions = [
             'is singing'
         ]
 
-places = ['at red fort.',
+places = [
+          'at red fort.',
           'at taj mahal.',
           'at india gate.',
           'at railway station.',
@@ -30,14 +37,38 @@ places = ['at red fort.',
         ]
 
 
+# create a list to store all generated headlines .
+generated_headline = []
+
+
+# While loop logic
 while True:
     name = random.choice(names)
     action = random.choice(actions)
     place = random.choice(places)
-    print(f"\nBREAKING NEWS : {name} {action} {place}")
+
+
+    headline = f"{name} {action} {place}"
+    generated_headline.append(headline)
+
+    print(f"\nBREAKING NEWS : {headline}")
     print("headline")
-    result = input("Do you want to continue? ").strip().lower()
+    result = input("Do you want to continue? (yes/no) ---> ").strip().lower()
 
     if result == 'no':
         break
+text_file = input("Do you want to download all this headlines in text file? (yes/no) ---> ").strip().lower()
+
+
+
+# text file download option logic
+if text_file == 'yes':
+    
+    with open("generated_headlines.txt", "w") as file:
+        for headline in generated_headline:
+            file.write(headline + '\n')
+    print("All headlines have been saved to generated_headlines.txt")
+
+
 print('\nThanks a lot for comming at Fake News Headline Generator')
+# print(generated_headline)
